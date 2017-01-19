94626b442db01564886af99accf44f0801e2c6c8
Serafin <leaf466@gmail.com>
2017-01-19 15:09:08 -0800

    WIP Testing db calls

--- models/Tickets.js
+++ models/Tickets.js
@@ -5,18 +5,11 @@ const db = require('../db/index.js')
 class Ticket{
   constructor(options={}){
 
-    const {owner, train, destination} = options
+    const {owner, train, destination_id, id} = options
 
     // Database matching
-    this._id = undefined
-    this.destination = destination
-
-
-    // Derived
-    this.owner = owner
-    this.train = train
-    // db.getPassengers()
-    db.update('ticket', this)
+    this._id = id
+    this.destination_id = destination_id
   }
 
   // Getters
@@ -29,14 +22,16 @@ class Ticket{
     this._id = newId
   }
 
-  // Methods
-
 }
 
 db.sequelize.sync().then( _ => {
   console.log("I\'M SYNCHED!");
-  let george = new Ticket()
-  console.log("George: ", george);
+  // let george = new Ticket()
+  let frankie = new Ticket({id: '3a43b299-6bf3-49c4-b2ad-b67a6f4d2ed4', destination_id: '3B43b299-6bf3-49c4-b3ad-b67a6f4d2ed4'})
+  // db.update('ticket', frankie)
+  db.findAll('ticket', 'id', '3a43b299-6bf3-49c4-b2ad-b67a6f4d2ed4', result => {
+    console.log("FIND RESULT:", result);
+  })
 })
 
 module.exports = {
